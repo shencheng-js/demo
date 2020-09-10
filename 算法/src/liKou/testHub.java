50aa0358b64748039ce10896cc625ed17a2ee5f7
shencheng <2283228123@qq.com>
2020-09-10 19:53:25 +0800

    testhub

---
+++ 算法/src/liKou/testHub.java
@@ -0,0 +1,11 @@
+package liKou;
+
+/**
+ * @author sc
+ * @date 2020/9/10
+ **/
+public class testHub {
+    public static void main(String[] args) {
+        System.out.println("This is test");
+    }
+}
